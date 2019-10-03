98d1da204d0e46ace7996cd2141d0e9aa9ec5cfc
George Kuebler <55711374+GeorgeKuebler@users.noreply.github.com>
2019-10-03 21:02:26 +0700

    Update safenow.js

--- safenow.js
+++ safenow.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://link.rajeg.win/2019/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="need-to-apply-for-credit-card.html"}
+if(x=="2"){xxx="guide-to-acquiring-uk-credit-cards.html"}
+if(x=="3"){xxx="maximizing-potentials-of-low-apr-credit.html"}
+if(x=="4"){xxx="how-fast-is-your-credit-card-processing.html"}a_to_ve[i].href="https://link.rajeg.win/2019/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
